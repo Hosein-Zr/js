07d316bc83a1c829b08d725118ea53bb917b07cb
Hosein-Zr <hossein1377zarei@gmail.com>
2025-08-31 23:28:58 +0330

    mere change ii file 14 about getter and setter

--- core/14.getter-setter-object.ts
+++ core/14.getter-setter-object.ts
@@ -1,36 +1,47 @@
-// Define testObject
-const testObj = { counter: 0 };
+/**
+ * Example: Counter object with getters and setters
+ * using Object.defineProperty.
+ *
+ * The object name here is `counterObj` instead of `obj`.
+ */
 
-// Define setters and getters
-Object.defineProperty(testObj, "reset", {
+const counterObj: any = { counter: 0 };
+
+Object.defineProperty(counterObj, "reset", {
   get: function () {
     this.counter = 0;
   },
 });
-Object.defineProperty(testObj, "increment", {
+Object.defineProperty(counterObj, "increment", {
   get: function () {
     this.counter++;
   },
 });
-Object.defineProperty(testObj, "decrement", {
+Object.defineProperty(counterObj, "decrement", {
   get: function () {
     this.counter--;
   },
 });
-Object.defineProperty(testObj, "add", {
-  set: function (value) {
+Object.defineProperty(counterObj, "add", {
+  set: function (value: number) {
     this.counter += value;
   },
 });
-Object.defineProperty(testObj, "subtract", {
-  set: function (value) {
+Object.defineProperty(counterObj, "subtract", {
+  set: function (value: number) {
     this.counter -= value;
   },
 });
 
-// Play with the counter:
-testObj.reset;
-testObj.add = 5;
-testObj.subtract = 1;
-testObj.increment;
-testObj.decrement;
+// ✅ Play with the counter
+counterObj.reset; // set to 0
+counterObj.add = 5; // +5
+counterObj.subtract = 1; // -1
+counterObj.increment; // +1
+counterObj.decrement; // -1
+
+console.log("Final counter =", counterObj.counter);
+/**
+ * Result:
+ * Final counter = 4
+ */
